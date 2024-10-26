3879ccaf40f2392ddbda328023659deceadf82d1
univero <1449610641@qq.com>
2024-10-26 15:32:43 +0800

    fix: 修正了依赖

--- provider/wire_gen.go
+++ provider/wire_gen.go
@@ -35,6 +35,7 @@ func NewProvider() (*Provider, error) {
 	}
 	chargeService := service.ChargeService{
 		ChargeClient: openapiCharge,
+		UserClient:   openapiUser,
 	}
 	keyService := service.KeyService{
 		KeyClient: openapiUser,
